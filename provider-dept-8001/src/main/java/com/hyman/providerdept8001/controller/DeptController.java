c9194511ddb2e1ea9550a8031aaf6d791187c029
hyman <hyman@163.com>
2022-02-05 15:53:22 +0800

    rename

--- provider-dept-8001/src/main/java/com/hyman/providerdept8001/controller/DeptController.java
+++ provider-dept-8001/src/main/java/com/hyman/providerdept8001/controller/DeptController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/dept")
 public class DeptController {
@@ -33,9 +32,37 @@ public class DeptController {
     @Autowired
     private DiscoveryClient client;
 
+    /**
+     * 一旦调用服务方法失败并抛出异常后，会自动调用 fallback 指定的方法。并且默认超时时间为 1 秒，这里设置为 3 秒。
+     *
+     * 如果希望将某些线程的本地上下文 context 传播到 @HystrixCommand 中，默认是不起作用的，因为它在线程池中执行命令（以防超时）。
+     * 您可以通过配置或直接在注释中切换 Hystrix 以使用与调用者相同的线程，方法是要求它使用不同的“隔离策略”。
+     * execution.isolation.strategy 有两个属性：
+     * 1，thread，context 会在一个隔离的线程中被执行，并且此时的并发请求数会受到线程池线程数的限制。它是默认的属性。
+     * 2，semaphore，信号量，context 会在当前的线程中被执行，并且此时的并发请求数会受到信号量大小的限制。
+     *
+     * 其含义是 HystrixCommand 注解的方法与 fallbackMethod 指定的方法，是否在同一个线程中被执行。
+     * 而且在正常可使用的情况下，是不需要配置的，直到抛出异常，找不到作用域上下文，此时则需要配置使用相同的线程。
+     *
+     * 还可以选择将 hystrix.shareSecurityContext 属性设置为true。这样会自动配置一个 Hystrix并发策略插件挂钩，以便将 SecurityContext
+     * 从主线程传输到 Hystrix 命令使用的线程。Hystrix 不允许注册多个 Hystrix并发策略，因此可以将自己的 Hystrix concurrency策
+     * 略声明为 Spring bean来使用扩展机制。Spring Cloud 会在 Spring 上下文中查找您的实现，并将其包装在自己的插件中。
+     * @return
+     */
     @GetMapping("/getById/{id}")
+    @HystrixCommand(
+            fallbackMethod = "hystrixMethod",
+            commandProperties ={
+                    @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="3000"),
+                    @HystrixProperty(name="execution.isolation.strategy", value="THREAD")
+            })
     public Department findById(@PathVariable("id") Integer id){
-        return deptService.findById(id);
+
+        Department department = deptService.findById(id);
+        if(null == department){
+            throw new RuntimeException("depart is null");
+        }
+        return department;
     }
 
     @PostMapping("/findAll")
@@ -68,5 +95,9 @@ public class DeptController {
 
         List<String> services = client.getServices();
         return services;
+    }
+
+    public Object hystrixMethod(@PathVariable("id") Integer id){
+        return "熔断异常！没有指定 ID 的数据："+id;
     }
 }
--- provider-dept-8001/src/main/java/com/hyman/providerdept8001/dao/DeptDao.java
+++ provider-dept-8001/src/main/java/com/hyman/providerdept8001/dao/DeptDao.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@mapper
 public interface DeptDao {
 
     boolean addDept(Department department);
